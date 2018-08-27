8394140587325f40f21ef4e496ccd20b2a8148ca
nateGeorge <wordsforthewise@gmail.com>
2018-08-27 17:52:16 -0600

    add todo

--- scrape_reuters_rss.py
+++ scrape_reuters_rss.py
@@ -434,3 +434,33 @@ local_time = dt.replace(tzinfo=pytz.utc).astimezone(local_timezone)
 # https://www.reuters.com/news/archive/hotStocksNews
 # https://www.reuters.com/news/archive
 # https://www.reuters.com/news/archive/rates-rss?view=page&page=6&pageSize=10
+
+
+# TODO: deal with lost connections:
+"""
+world
+---------------------------------------------------------------------------
+KeyError                                  Traceback (most recent call last)
+~/github/get_news/scrape_reuters_rss.py in <module>
+    415
+    416 if __name__ == "__main__":
+--> 417     continually_scrape_rss()
+    418
+    419 # to convert timezone
+
+~/github/get_news/scrape_reuters_rss.py in continually_scrape_rss()
+     75             print(l)
+     76             feeds = feedparser.parse(f)
+---> 77             if feeds['status'] != 200:
+     78                 print('status is not good:', str(feeds['status']) + '; exiting')
+     79                 break
+
+/usr/lib/python3/dist-packages/feedparser.py in __getitem__(self, key)
+    381             elif dict.__contains__(self, realkey):
+    382                 return dict.__getitem__(self, realkey)
+--> 383         return dict.__getitem__(self, key)
+    384
+    385     def __contains__(self, key):
+
+KeyError: 'status'
+"""
